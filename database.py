c1f4570f2b2d11a495251b77245e8dd53beadae2
Trần An Khang <tranankhang25022011@gmail.com>
2026-01-03 01:58:20 +0700

    Fix SQLAlchemy mapper error (remove DeclarativeBase)

--- database.py
+++ database.py
@@ -1,7 +1,4 @@
+# database.py
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import DeclarativeBase
 
-class Base(DeclarativeBase):
-    pass
-
-db = SQLAlchemy(model_class=Base)
+db = SQLAlchemy()
